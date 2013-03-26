4db93e6af1201c6d86df528edc6de053ad62965f
Jacek Wojcik <guitarplayer@op.pl>
2013-03-26 15:30:13 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MemWordia::Application.config.secret_token = 'af919df3771ee202dd34b81fdab8d7fe4438dea487c7ed971fcd9333c2c3cb84c663e5f9ad48b5557a16819fae797b3195b355ed7a64bb46de198d4837913afe'
